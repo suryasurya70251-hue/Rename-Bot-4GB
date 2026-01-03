b90130ef15bb9e3d098c35f47ae1d0795df87b88
suryasurya70251-hue <suryasurya70251@gmail.com>
2026-01-03 13:05:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "30510149"))
+API_HASH = os.environ.get("API_HASH", "40863e0a77fd80789861c05a7e8a34bd")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8283669513:AAHfNq4Ydg5h4v-CKxbVYCoEk0MoSOINswU")
+ADMIN = int(os.environ.get("ADMIN", "7667050661"))
 
 
 # Premium 4GB Renaming Client Config
@@ -14,13 +14,13 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1003656942560")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003644203182"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://filesharing1:filesharing1@filesharing1.eqtx1sn.mongodb.net/?appName=Filesharing1")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "filesharing1")
 
 
 # Other Variables Config
@@ -38,8 +38,8 @@ START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d
 # Token Verification Adding Features Is Paid So If You Want Then Dm Me
 
 
-# SHORTNER_URL = os.environ.get("SHORTNER_URL", "")
-# SHORTNER_API = os.environ.get("SHORTNER_API", "")
+# SHORTNER_URL = os.environ.get("SHORTNER_URL", "https://arolinks.com")
+# SHORTNER_API = os.environ.get("SHORTNER_API", "24fdaab6c2ef3f44a8d671dba1dfc693bba5fa35")
 # TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
 
 
